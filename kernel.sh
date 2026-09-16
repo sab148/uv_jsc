dfdab2ec612c27fbf1b13f614ad0aeb944583dd2
benassou1 <s.benassou@fz-juelich.de>
2026-09-16 10:22:28 +0200

    add path env

--- kernel.sh
+++ kernel.sh
@@ -6,5 +6,7 @@ export PYTHONNOUSERSITE=1
 
 REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+export PATH="$REPO_DIR/.venv/bin:$PATH"
+
 exec "$REPO_DIR/.venv/bin/python" \
     -m ipykernel_launcher "$@"
